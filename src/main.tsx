45b29b5cfea777c3d63ac5cc19818fe2eb1171d9
taejeong77 <oleh13579@naver.com>
2026-06-04 17:06:39 +0900

    fix: 스캔 프로그레스바 및 페이지 전환 애니메이션 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { Toaster } from "react-hot-toast";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,5 +7,23 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster
+  position="top-center"
+  toastOptions={{
+    success: {
+      style: {
+        background: "#1e293b",
+        color: "#fff",
+        borderRadius: "12px",
+        fontSize: "14px",
+        padding: "12px 16px",
+      },
+      iconTheme: {
+        primary: "#60a5fa",
+        secondary: "#fff",
+      },
+    },
+  }}
+/>
   </StrictMode>,
 )
